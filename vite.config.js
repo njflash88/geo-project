2778cf8b7c3b697e2188f532e2fbe05145383462
njflash88 <gordonchu88@yahoo.com>
2025-08-15 05:17:19 +0800

    deploy modify base:/geo-project 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/geo-project",
+  base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: true, // Helpful for debugging
+  },
 });
